af99d712229405cc9df8eccac9121c2d4c5a21b4
Fatimah Ismael <fatimahismael@yahoo.com>
2023-04-27 09:20:18 -0700

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,6 +1,20 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const jwt = require('jsonwebtoken')
 
 const restricted = (req, res, next) => {
+  const token = req.headers.authorization
+    if(token){
+      jwt.verify(token, JWT_SECRET,(err, decoded)=>{
+        if(err){
+          next({ status: 401, message: 'Token invalid'})
+        } else if(!token){
+          next({ status: 401, message: 'Token required'})
+        } else{
+          next()
+        }
+      })
+    }
+
   /*
     If the user does not provide a token in the Authorization header:
     status 401
@@ -16,9 +30,15 @@ const restricted = (req, res, next) => {
 
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
+ next()
 }
 
 const only = role_name => (req, res, next) => {
+  if(!token){
+    next({status: 403, message: 'This is not for you'})
+  } else {
+    next()
+  }
   /*
     If the user does not provide a token in the Authorization header with a role_name
     inside its payload matching the role_name passed to this function as its argument:
@@ -29,10 +49,16 @@ const only = role_name => (req, res, next) => {
 
     Pull the decoded token from the req object, to avoid verifying it again!
   */
+ next()
 }
 
 
 const checkUsernameExists = (req, res, next) => {
+  if(!req.body.username){
+    next({status: 401, message: 'Invalid credentials'})
+  } else {
+    next()
+  }
   /*
     If the username in req.body does NOT exist in the database
     status 401
@@ -40,10 +66,22 @@ const checkUsernameExists = (req, res, next) => {
       "message": "Invalid credentials"
     }
   */
+
 }
 
 
 const validateRoleName = (req, res, next) => {
+  if (!req.body.role_name || !req.body.role_name.trim()) {
+    req.role_name = 'student'
+    next()
+  } else if(req.body.role_name.trim()=== 'admin'){
+    next({ status: 422, message: "Role name can not be admin" })
+  } else if(req.body.role_name.trim().length > 32 ){
+    next({ status:422, message: 'Role name can not be longer than 32 chars'})
+  } else {
+    req.role_name = 'student' || req.body.role_name.trim()
+    next()
+  }
   /*
     If the role_name in the body is valid, set req.role_name to be the trimmed string and proceed.
 
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -2,7 +2,16 @@ const db = require('../../data/db-config.js');
 
 function find() {
   return db('users')
+  .join('roles', 'users.role_id', 'roles.role_id')
+  .select('user_id', 'username', 'role_name')
   /**
+   * select
+ user_id,
+ username,
+ role_name
+ from users
+ join roles on 
+ users.role_id = roles.role_id
     You will need to join two tables.
     Resolves to an ARRAY with all users.
 
@@ -22,7 +31,20 @@ function find() {
 }
 
 function findBy(filter) {
+  return db('users')
+  .join('roles', 'users.role_id', 'roles.role_id')
+  .select('user_id', 'username', 'password', 'role_name')
+  .where(filter)
   /**
+   * select
+ user_id,
+ username,
+ password,
+ role_name
+ from users
+ join roles on 
+ users.role_id = roles.role_id
+ where users.user_id = 1;
     You will need to join two tables.
     Resolves to an ARRAY with all users that match the filter condition.
 
@@ -38,6 +60,10 @@ function findBy(filter) {
 }
 
 function findById(user_id) {
+  return db('users')
+  .join('roles', 'users.role_id', 'roles.role_id')
+  .select('user_id', 'username', 'role_name')
+  .where('users.user_id',user_id).first()
   /**
     You will need to join two tables.
     Resolves to the user with the given user_id.
